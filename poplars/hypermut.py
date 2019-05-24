fc463e341562fc5c75c898f683949fc92fc5e3d2
kwade4 <kwade4@uwo.ca>
2019-05-24 17:04:45 -0400

    -Code clean-up and refactoring -Fix #3

--- poplars/hypermut.py
+++ poplars/hypermut.py
@@ -12,8 +12,8 @@ import scipy.stats as stats
 
 class HyperMut:
 
-    def __init__(self, seq_name, num_muts, potential_muts, ctrl_muts,
-                 potential_ctrls, rate_ratio, p_value, odds_ratio, ctable):
+    def __init__(self, seq_name=None, num_muts=None, potential_muts=None, ctrl_muts=None,
+                 potential_ctrls=None, rate_ratio=None, p_value=None, odds_ratio=None, ctable=None):
         self.seq_name = seq_name
         self.num_muts = num_muts
         self.pot_muts = potential_muts
@@ -41,6 +41,40 @@ def parse_args():
     return parser.parse_args()
 
 
+def make_ctable(seq, gees):
+    """
+    Creates a list of contingency tables for each query sequence
+    :param seq: the query sequence
+    :param gees: list of positions of G's in the reference sequence
+    :return ctable: a contingency table for the query sequence
+    """
+
+    # Keele et al. apply Fisher's exact test to a contingency table
+    ctable = [
+        [0, 0],  # (G->A, G) in GRD context
+        [0, 0]  # in other contexts (GYN|GRC is !GRD)
+    ]
+
+    # Matches downstream RD motif in query sequence
+    ds_motifs = [match.start() for match in mut.finditer(seq)]
+    # Matches downstream YN|RC motif in query sequence
+    ds_ctrl = [match.start() for match in ctrl.finditer(seq)]
+
+    # Iterate through all positions with G in consensus
+    for i in gees:
+        nt = seq[i]  # Corresponding base in query sequence
+
+        # G->A mutation in downstream
+        row = (0 if nt == 'A' else 1)
+        if i in ds_motifs:
+            ctable[0][row] += 1
+
+        elif i in ds_ctrl:
+            ctable[1][row] += 1
+
+    return ctable
+
+
 def hypermut(infile):
     # Get reference sequence
     with open(infile) as handle:
@@ -50,68 +84,39 @@ def hypermut(infile):
         print("skipping first {} records".format(args.skip))
         fasta = fasta[int(args.skip):]
 
-    # Open and clear output file
-    with open("hypermut_output.txt", "w+") as outfile:
-        outfile.truncate(0)
-
     refseq = fasta[0][1]  # First sequence is the reference sequence
 
     # Locate GRD motifs in reference sequence
     gees = [i for i, nt in enumerate(refseq) if nt.upper() == 'G']
 
-    results = [[0] * 8 for _ in range(len(fasta) - 1)]
-    ctable_list = []
+    results = []
 
     # Iterate through sequences and identify substitutions from consensus
     for j, seq in enumerate(fasta[1:]):
         header = seq[0]
-        s = seq[1]
+        ctable= make_ctable(seq[1], gees)
 
-        # Keele et al. apply Fisher's exact test to a contingency table
-        ctable = [
-            [0, 0],  # (G->A, G) in GRD context
-            [0, 0]  # in other contexts (GYN|GRC is !GRD)
-        ]
+        result = HyperMut()
 
-        # Matches downstream RD motif in query sequence
-        ds_motifs = [match.start() for match in mut.finditer(s)]
-        # Matches downstream YN|RC motif in query sequence
-        ds_ctrl = [match.start() for match in ctrl.finditer(s)]
-
-        # Iterate through all positions with G in consensus
-        for i in gees:
-            nt = s[i]  # Corresponding base in query sequence
-
-            # G->A mutation in downstream
-            row = (0 if nt == 'A' else 1)
-            if i in ds_motifs:
-                ctable[0][row] += 1
-
-            elif i in ds_ctrl:
-                ctable[1][row] += 1
-        ctable_list.append(ctable)
-
-        for i in range(len(ctable_list)):
-            # Populate output table using entries from contingency table
-            results[j][0] = header
-            results[j][1] = ctable_list[i][0][0]  # Mutation sites
-            results[j][2] = ctable_list[i][0][0] + ctable_list[i][0][1]  # Potential mutation sites
-            results[j][3] = ctable_list[i][1][0]  # Control mutations
-            results[j][4] = ctable_list[i][1][0] + ctable_list[i][1][1]  # Potential controls
-            results[j][5] = rate_ratio(ctable_list[i])  # Rate ratio
+        # Populate output table using entries from contingency table
+        result.seq_name = header
+        result.num_muts = ctable[0][0]  # Mutation sites
+        result.pot_muts = ctable[0][0] + ctable[0][1]  # Potential mutation sites
+        result.ctrl_muts = ctable[1][0]  # Control mutations
+        result.potential_ctrls = ctable[1][0] + ctable[1][1]  # Potential controls
+        result.rate_ratio = rate_ratio(ctable)  # Rate ratio
 
         # Calculate one-sided P-value
         odds_ratio, p_value = stats.fisher_exact(ctable, alternative='greater')
-        results[j][6] = p_value  # Fisher's exact P-value
-        results[j][7] = odds_ratio
+        result.p_value = p_value  # Fisher's exact P-value
+        result.odds_ratio = odds_ratio
+        result.ctable = ctable
 
-        make_data_file(header, s, j + 1, gees, ds_motifs, ds_ctrl)
+        results.append(result)
 
-    print(ctable_list)
-    # pretty_print(results)
-    results_list = make_return(results, ctable_list)
+        # make_data_file(header, seq[1], j + 1, gees, ds_motifs, ds_ctrl)
 
-    return results_list
+    return results
 
 
 def rate_ratio(ctable):
@@ -189,32 +194,6 @@ def make_data_file(header, s, num_seq, gees, ds_motifs, ds_ctrl):
                     str(i + 1),
                     '1' if nt == 'A' else '0'
                 ))
-
-
-def make_return(results_table, ctable_list):
-    """
-    Creates a list of HyperMut objects with attributes specified from results_table and ctable_list
-    :param results_table: the table of results from hypermut
-    :param ctable_list: list of contingency tables
-    :return: a list of HyperMut objects
-    """
-
-    tmp_list = []
-    # Make a list of objects
-    for i in range(len(results_table)):
-        tmp = HyperMut(results_table[i][0], results_table[i][1], results_table[i][2],
-                       results_table[i][3], results_table[i][4], results_table[i][5],
-                       results_table[i][6], results_table[i][7], ctable_list[i])
-        tmp_list.append(tmp)
-
-    results_list = []
-    # Make a list of iterable objects
-    for i in range(len(tmp_list)):
-        result = vars(tmp_list[i])
-        results_list.append(result)
-
-    print(results_list)
-    return results_list
 
 
 if __name__ == '__main__':
