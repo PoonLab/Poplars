79342654ad70b4c2b94440d862aff16c275c1d54
Kaitlyn Wade <48563808+kwade4@users.noreply.github.com>
2019-05-29 10:18:42 -0400

    Merge dev into master (#7)
    * Delete poisson_fitter.py
    
    * Implemented command line interface
    
    * Updated command line interface
    
    -Cleaned up code for command line interface
    -Partially implemented 'align'
    
    * Updated argument parsing
    
    * -Updated path to MAFFT executable
    -Fixes #6
    
    * -Created HyperMut class to fix #3
    -Fixed comments
    
    * -Code clean-up and refactoring
    -Fix #3
    
    * -Unit tests for rate_ratio
    
    * Refactored Code
    
    * -Added MAFFT binaries for Mac (all-in-one version 7.427) and Windows (all-in-one version 7.428)

--- poplars/hypermut.py
+++ poplars/hypermut.py
@@ -4,10 +4,52 @@ as described in Keele et al. http://www.pnas.org/cgi/content/short/0802203105
 Supplementary Materials.
 """
 
-import re
 import argparse
-from Bio import SeqIO
+import re
+
 import scipy.stats as stats
+from Bio import SeqIO
+
+
+class MutationInfo:
+    """
+    Represents mutation information about how a query sequence compares to a reference sequence
+    """
+
+    def __init__(self, seq_name=None, num_muts=None, potential_muts=None, ctrl_muts=None,
+                 potential_ctrls=None, rate_ratio=None, p_value=None, odds_ratio=None,
+                 ctable=None, mut_sites=None, ctrl_sites=None):
+        """
+        Generate mutation information for a query sequence
+        :param seq_name: the name of the sequence
+        :param num_muts: the number of mutated sites, compared to the reference sequence
+        :param potential_muts: the number of potential mutation sites (GRD)
+        :param ctrl_muts: the number of control sites
+        :param potential_ctrls: the number of potential control sites (GYN|GRC)
+        :param rate_ratio: the rate ratio for the mutation
+        :param p_value: the p-value
+        :param odds_ratio: the odds ratio
+        :param ctable: a contingency table
+        :param mut_sites: <dict> a dictionary where the keys are the locations of potential
+                            mutation sites (GRD motifs) and the values are 1 if the sequence
+                            is mutated; 0 otherwise
+        :param ctrl_sites: <dict> a dictionary where the keys are the locations of potential
+                            control sites (GYN|GRC) and the values are 1 if the sequence is
+                            mutated; 0 otherwise
+        """
+
+        self.seq_name = seq_name
+        self.num_muts = num_muts
+        self.pot_muts = potential_muts
+        self.ctrl_muts = ctrl_muts
+        self.potential_ctrls = potential_ctrls
+        self.rate_ratio = rate_ratio
+        self.p_value = p_value
+        self.odds_ratio = odds_ratio
+        self.ctable = ctable
+        self.mut_sites = mut_sites
+        self.ctrl_sites = ctrl_sites
+
 
 mut_pattern = '[AGCT](?=[AG][AGT])'
 mut = re.compile(mut_pattern)  # Matches potential mutation sites (GRD)
@@ -22,79 +64,104 @@ def parse_args():
     )
     parser.add_argument('fasta', help="<input> path to FASTA file")
     parser.add_argument('--skip', help="<option> number of records to skip")
+    parser.add_argument('--out', help="<option> write output to the specified file")
+
     return parser.parse_args()
 
 
-def hypermut(infile):
-    # Get reference sequence
+def make_results(seq, gees):
+    """
+    Creates a list of contingency tables for each query sequence
+    :param seq: the query sequence as a tuple of the form (name, sequence)
+    :param gees: list of positions of G's in the reference sequence
+    :return ctable: a contingency table for the query sequence
+    """
+
+    # Keele et al. apply Fisher's exact test to a contingency table
+    ctable = [
+        [0, 0],  # (G->A, G) in GRD context
+        [0, 0]  # in other contexts (GYN|GRC is !GRD)
+    ]
+
+    s = seq[1]  # query sequence
+    # Matches downstream RD motif in query sequence
+    ds_motifs = [match.start() for match in mut.finditer(s)]
+    # Matches downstream YN|RC motif in query sequence
+    ds_ctrl = [match.start() for match in ctrl.finditer(s)]
+
+    mut_sites = {}
+    ctrl_sites = {}
+    # Iterate through all positions with G in consensus
+    for i in gees:
+        nt = s[i]  # Corresponding base in query sequence
+
+        # G->A mutation in downstream
+        row = (0 if nt == 'A' else 1)
+        if i in ds_motifs:
+            ctable[0][row] += 1
+            # Populate dictionary with locations of mutated sites
+            mut_sites[(i + 1)] = 1 if nt == 'A' else 0
+
+        elif i in ds_ctrl:
+            ctable[1][row] += 1
+            # Populate dictionary with locations of control sites
+            ctrl_sites[(i + 1)] = 1 if nt == 'A' else 0
+
+    result = MutationInfo()
+
+    # Set attributes of MutationInfo
+    result.seq_name = seq[0]  # header
+    result.num_muts = ctable[0][0]  # Mutation sites
+    result.pot_muts = ctable[0][0] + ctable[0][1]  # Potential mutation sites
+    result.ctrl_muts = ctable[1][0]  # Control mutations
+    result.potential_ctrls = ctable[1][0] + ctable[1][1]  # Potential controls
+    result.rate_ratio = rate_ratio(ctable)  # Rate ratio
+
+    odds_ratio, p_value = stats.fisher_exact(ctable, alternative='greater')  # Calculate one-sided P-value
+    result.p_value = p_value  # Fisher's exact P-value
+    result.odds_ratio = odds_ratio
+    result.ctable = ctable
+
+    result.mut_sites = mut_sites
+    result.ctrl_sites = ctrl_sites
+
+    return result
+
+
+def hypermut(infile, skip=None):
+    """
+    Determines if a sequence is hypermutated compared to the reference sequence
+    :param infile: the input FASTA file
+    :param skip: the number of records to skip
+    :return results: a list of MutationInfo Objects
+    """
+
     with open(infile) as handle:
         fasta = [list(s) for s in SeqIO.FastaIO.SimpleFastaParser(handle)]
-    if args.skip:
-        print ("skipping first {} records".format(args.skip))
-        fasta = fasta[int(args.skip):]
 
-    # Open and clear output file
-    with open("hypermut_output.txt", "w+") as outfile:
-        outfile.truncate(0)
+    if skip:
+        print("skipping first {} records".format(skip))
+        fasta = fasta[int(skip):]
 
     refseq = fasta[0][1]  # First sequence is the reference sequence
-
-    # Locate GRD motifs in reference sequence
-    gees = [i for i, nt in enumerate(refseq) if nt.upper() == 'G']
-
-    results = [[0] * 8 for _ in range(len(fasta) - 1)]
+    gees = [i for i, nt in enumerate(refseq) if nt.upper() == 'G']  # Locate GRD motifs in reference sequence
+    results = []
 
     # Iterate through sequences and identify substitutions from consensus
     for j, seq in enumerate(fasta[1:]):
-        header = seq[0]
-        s = seq[1]
+        result = make_results(seq, gees)
+        results.append(result)
 
-        # Keele et al. apply Fisher's exact test to a contingency table
-        ctable = [
-            [0, 0],  # (G->A, G) in GRD context
-            [0, 0]  # in other contexts (GYN|GRC is !GRD)
-        ]
-
-        # Matches downstream RD motif in query sequence
-        ds_motifs = [match.start() for match in mut.finditer(s)]
-        # Matches downstream YN|RC motif in query sequence
-        ds_ctrl = [match.start() for match in ctrl.finditer(s)]
-
-        # Iterate through all positions with G in consensus
-        for i in gees:
-            nt = s[i]  # Corresponding base in query sequence
-
-            # G->A mutation in downstream
-            row = (0 if nt == 'A' else 1)
-            if i in ds_motifs:
-                ctable[0][row] += 1
-
-            elif i in ds_ctrl:
-                ctable[1][row] += 1
-
-        # Populate output table using entries from contingency table
-        results[j][0] = header
-        results[j][1] = ctable[0][0]  # Mutation sites
-        results[j][2] = ctable[0][0] + ctable[0][1]  # Potential mutation sites
-        results[j][3] = ctable[1][0]  # Control mutations
-        results[j][4] = ctable[1][0] + ctable[1][1]  # Potential controls
-        results[j][5] = rate_ratio(ctable)  # Rate ratio
-
-        # Calculate one-sided P-value
-        odds_ratio, p_value = stats.fisher_exact(ctable, alternative='greater')
-        results[j][6] = p_value  # Fisher's exact P-value
-        results[j][7] = odds_ratio
-
-        make_data_file(header, s, j + 1, gees, ds_motifs, ds_ctrl)
-
-    pretty_print(results)
+    return results
 
 
 def rate_ratio(ctable):
     """
     Calculate rate ratio from contingency table
-    @param ctable: Contingency table...
+    :param ctable: Contingency table
+    :return r_ratio: rate ratio
     """
+
     muts = float(ctable[0][0])
     pot_muts = float(muts + ctable[0][1])
     ctrl_muts = float(ctable[1][0])
@@ -108,58 +175,56 @@ def rate_ratio(ctable):
     return r_ratio
 
 
-def pretty_print(table):
+def pretty_print(results):
     """
-    Print headings in bold
+    Print results
+    :param results: a list of MutationInfo objects
     """
     print("\033[1m{0:<11} {1:<7} {2:<22} {3:<15} {4:<21} {5:<22} {6:<25} {7:<20}".format
           ("Sequence", "Muts", "Potential Mut Sites", "Control Muts", "Potential Controls",
            "Rate Ratio", "Fisher's Exact P-value", "Odds Ratio\033[0m"))
 
     # Print values of rows under corresponding headings
-    for i in range(len(table)):
-        print("{0:<11} {1:<7} {2:<22} {3:<15} {4:<21} {5:<22} {6:<25} {7:<20}".format(*table[i]))
+    for result in results:
+        print("{0:<11} {1:<7} {2:<22} {3:<15} {4:<21} {5:<22} {6:<25} {7:<20}"
+              .format(result.seq_name, result.num_muts, result.pot_muts,
+                      result.ctrl_muts, result.potential_ctrls,
+                      result.rate_ratio, result.p_value, result.odds_ratio))
 
     # Print summary of hypermutated sequences
     print("\n\033[1m\033[4mSummary:\033[0m")
-    for i in range(len(table)):
-        if table[i][6] <= 0.05:
-            print("{} appears to be hypermutated (OR={})".format(table[i][0], table[i][7]))
+    for result in results:
+        if result.p_value <= 0.05:
+            print("{} appears to be hypermutated (OR={})".format(result.seq_name, result.odds_ratio))
 
 
-def make_data_file(header, s, num_seq, gees, ds_motifs, ds_ctrl):
-    # Write hypermut data to a file
-    with open("hypermut_output.txt", "a") as output:
-        if num_seq == 1:
-            output.write("Regex: " + mut_pattern + ", " + ctrl_pattern + "\n")
-        output.write("\nSequence Number: " + str(num_seq))
-        output.write("\nSequence Name: " + header)
-        output.write("\nPos\tMut\n")
+def make_data_file(file_name, mutation_info_list):
+    """
+    Writes detailed output of hypermut to a text file
+    :param file_name: name of the output file
+    :param mutation_info_list: list of MutationInfo objects
+    """
 
-        for i in gees:
-            nt = s[i]
-            if i in ds_motifs:
-                if nt == 'A':
-                    output.write(str(i + 1) + "\t" + "1\n")
-                else:
-                    output.write(str(i + 1) + "\t" + "0\n")
+    with open(file_name, "w+") as output:
+        for mutation_info in mutation_info_list:
+            output.write("\nSequence Name: {}".format(mutation_info.seq_name))
+            output.write("\nPos\tMut\n")
+            for key in mutation_info.mut_sites:
+                output.write("{}\t{}\n".format(key, mutation_info.mut_sites[key]))
 
-        output.write("\nSequence Name: " + header + " control")
-        output.write("\nPos\tMut\n")
+            output.write("\nSequence Name: {} control".format(mutation_info.seq_name))
+            output.write("\nPos\tMut\n")
+            for key in mutation_info.ctrl_sites:
+                output.write("{}\t{}\n".format(key, mutation_info.ctrl_sites[key]))
 
-        for i in gees:
-            nt = s[i]
-            if i in ds_ctrl:
-                output.write("{}\t{}\n".format(
-                    str(i+1), 
-                    '1' if nt=='A' else '0'
-                ))
-                #if nt == 'A':
-                #    output.write(str(i + 1) + "\t" + "1\n")
-                #else:
-                #    output.write(str(i + 1) + "\t" + "0\n")
+
+def main():
+    args = parse_args()
+    results = hypermut(args.fasta, args.skip)
+    if args.out:
+        make_data_file(args.out, results)
+    pretty_print(results)
 
 
 if __name__ == '__main__':
-    args = parse_args()
-    hypermut(args.fasta)
+    main()
---
+++ poplars/sequence_locator.py
@@ -0,0 +1,220 @@
+"""
+Python implementation of HIV Sequence Locator from https://www.hiv.lanl.gov
+"""
+
+import argparse
+import subprocess
+import tempfile
+import os
+import logging
+
+
+from Bio import SeqIO
+
+
+DNA_ALPHABET = "atgc"
+AA_ALPHABET = "ARDNCEQGHILKMFPSTWYV"
+
+
+def parse_args():
+    """
+    Parses command line arguments
+    """
+
+    bases = ["nucl", "prot"]
+    ref_organisms = ["K03455.fasta", "M33262.fasta"]
+
+    regions = ["LTR5", "Gag", "Matrix", "p17/p15", "Capsid",
+               "p24/p27", "p2", "Nucleocapsid", "p7/p8", "p1",
+               "p6", "GagPolTF", "Protease", "RT", "RNase",
+               "Integrase", "Vif", "Vpx", "Vpr", "Tat(exons)",
+               "Tat(exon1)", "Tat(exon2)", "Rev(exons)",
+               "Rev(exon1)", "Rev(exon2)", "Vpu", "Env",
+               "gp160", "gp120", "gp41", "Nef", "LTR3"
+               ]
+
+    parser = argparse.ArgumentParser(
+        description='Aligns a nucleotide or protein sequence relative to the HIV or SIV reference genomes; '
+                    'or retrieves a sequence in the HXB2 or SIVmm239 reference genome from its coordinates',
+    )
+    subparsers = parser.add_subparsers(dest='subcommand')
+
+    # Create subparser for 'align' mode
+    parser_align = subparsers.add_parser('align',
+                                         description='Align a nucleotide or protein sequence '
+                                                     'relative to the HIV or SIV reference genome',
+                                         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser_align.add_argument('file',
+                              help='Path to the input file.',
+                              )
+    parser_align.add_argument('base',
+                              help='Sequence base type. Allowed bases are: ' + ', '.join(bases),
+                              metavar='base',
+                              choices=bases
+                              )
+    parser_align.add_argument('-reference',
+                              type=argparse.FileType('r+'),
+                              help='FASTA file with the reference genome of HIV strain HXB2 (Accession: K03455) or '
+                                   'SIV strain SIVmm239 (Accession: M33262), or any other reference genome.',
+                              metavar=''
+                              )
+
+    # Create subparser for 'retrieve' mode
+    parser_retrieve = subparsers.add_parser('retrieve',
+                                            description='Retrieve a sequence in HXB2 '
+                                                        'or SIVmm239 from its coordinates',
+                                            formatter_class=argparse.ArgumentDefaultsHelpFormatter
+                                            )
+    parser_retrieve.add_argument('-reference',
+                                 type=argparse.FileType('r'),
+                                 help='FASTA file with reference genomes of HIV strain HXB2 (Accession: K03455) or SIV '
+                                      'strain SIVmm239 (Accession: M33262). Options are: ' + ', '.join(ref_organisms),
+                                 default="auto",
+                                 metavar='',
+                                 )
+    parser_retrieve.add_argument('-first',
+                                 default="start",
+                                 help='Starting coordinate of the genomic region'
+                                 )
+    parser_retrieve.add_argument('-last',
+                                 default="end",
+                                 help='Ending coordinate of the genomic region'
+                                 )
+    parser_retrieve.add_argument('-region',
+                                 help='List of case sensitive genomic regions. '
+                                      'Allowed regions are: ' + ', '.join(regions),
+                                 metavar='',
+                                 choices=regions
+                                 )
+
+    return parser.parse_args()
+
+
+def is_fasta(infile):
+    """
+    Checks whether an input file is a FASTA file
+    :param infile: the input file
+    :return: <true> if the file is a FASTA file; <false> otherwise
+    """
+
+    fasta = SeqIO.parse(infile, "fasta")
+    return any(fasta)
+
+
+def make_fasta(handle):
+    fasta = []
+    seq = ""
+    for line in handle:
+        if line.startswith('>'):
+            header = line
+            if len(seq) > 0:
+                fasta.append([header, seq])
+                seq = ""
+                header = line.strip('>\n')
+            else:
+                seq += line.strip('\n').upper()
+
+    fasta.append([header, seq])
+    return fasta
+
+
+def valid_sequence(base, query):
+    """
+    Verifies that input sequence uses the correct output
+    :param base: the base of the sequence (nucl or prot)
+    :param query: input sequence
+    :return: <true> if the input sequence uses the correct alphabet; <false> otherwise
+    """
+
+    if base == "nucl":
+        for pos in query:
+            if pos not in DNA_ALPHABET:
+                print("Invalid nucleotide sequence input")
+                return False
+        return True
+    else:
+        for pos in query:
+            if pos not in AA_ALPHABET:
+                print("Invalid amino acid sequence")
+                return False
+        return True
+
+
+def align(ref_seq, query):
+    """
+    Pairwise alignment of input sequence relative to HIV reference genome
+    :param: reference sequence
+    :param: query sequence
+    :return: output of alignment
+    """
+    with tempfile.NamedTemporaryFile('w+', delete=True) as handle:
+        handle.write(ref_seq)
+        handle.write('\n>query sequence\n{}\n'.format(query))
+
+        # Path for the temporary file
+        file_path = os.path.join(tempfile.gettempdir(), handle.name)
+
+        # Path to find MAFFT
+        script_path = os.path.dirname(os.path.abspath(__file__))
+        bin_path = os.path.join(script_path, 'bin/mafft-linux64/mafft.bat')
+
+        if not os.path.isfile(bin_path):
+            logging.error("No file exists.")
+
+        raw_output = subprocess.check_output([bin_path, '--quiet', file_path])
+
+    output = raw_output.decode('utf-8')
+    return output
+
+
+def sequence_align(ref_seq, base, infile):
+    """
+    Sequence locator for 'align mode'
+    :param ref_seq: reference sequence
+    :param base: base of the sequence (nucleotide or protein)
+    :param infile: input file containing query sequence
+    :return: the alignment of the query sequence with the reference genome
+    """
+
+    with open(infile) as handle:
+        if base == "nucl":
+            query = handle.read().lower()
+        else:
+            query = handle.read().upper()
+
+        # Check if query uses the correct alphabet
+        if valid_sequence(base, query):
+            align(ref_seq, query)
+
+
+def sequence_retrieve(ref_seq, region, start_coord, end_coord):
+    """
+    Sequence locator for 'retrieve' mode
+    :param ref_seq: reference genome sequence
+    :param region: genomic region
+    :param start_coord: starting coordinate
+    :param end_coord: ending coordinate
+    :return: return the genomic region defined by the starting and ending coordinates
+    """
+
+    return None
+
+
+def main():
+    args = parse_args()
+    ref_seq = args.reference.read()
+
+    if args.subcommand == "align":
+        base = args.base
+        infile = args.file
+        sequence_align(ref_seq, base, infile)
+
+    else:
+        region = args.region
+        start_coord = args.first
+        end_coord = args.last
+        sequence_retrieve(ref_seq, region, start_coord, end_coord)
+
+
+if __name__ == '__main__':
+    main()
