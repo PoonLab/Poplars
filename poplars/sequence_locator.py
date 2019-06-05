25e88f77a6425b0d750c81020d516290bec3ef99
kwade4 <kwade4@uwo.ca>
2019-06-05 16:42:55 -0400

    Refactored code and updated documentation

--- poplars/sequence_locator.py
+++ poplars/sequence_locator.py
@@ -3,7 +3,6 @@ Python implementation of HIV Sequence Locator from https://www.hiv.lanl.gov
 """
 
 import argparse
-import subprocess
 import tempfile
 import os
 from mafft import *
@@ -13,7 +12,6 @@ def parse_args():
     """
     Parses command line arguments
     """
-
     bases = ["nucl", "prot"]
 
     regions = ["LTR5", "Gag", "Matrix", "p17/p15", "Capsid",
@@ -36,8 +34,8 @@ def parse_args():
                                          description='Align a nucleotide or protein sequence '
                                                      'relative to the HIV or SIV reference genome',
                                          formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser_align.add_argument('infile',
-                              help='Path to the input file.',
+    parser_align.add_argument('query',
+                              help='Path to the query file.',
                               )
     parser_align.add_argument('base',
                               help='Sequence base type. Allowed bases are: ' + ', '.join(bases),
@@ -70,6 +68,11 @@ def parse_args():
                                  help='The reference virus',
                                  metavar='',
                                  choices=['hiv', 'siv']
+                                 )
+    parser_retrieve.add_argument('base',
+                                 description='Sequence base type. Allowed bases are: ' + ', '.join(bases),
+                                 metavar='base',
+                                 choices=bases
                                  )
     parser_retrieve.add_argument('--ref',
                                  help='FASTA file with the reference genome',
@@ -108,96 +111,102 @@ def valid_sequence(query, base):
     dna_alphabet = 'atgc'
     aa_alphabet = 'ARDNCEQGHILKMFPSTWYV'
 
-    valid = False
     if not query:
-        return valid
+        raise ValueError("Invalid sequence: sequence length is 0")
 
     if base == "nucl":
         # Nucleotide sequences are converted to lowercase
         query = query.lower()
         valid = all(pos in dna_alphabet for pos in query)
         if not valid:
-            print("Invalid nucleotide sequence: {}".format(query))
+            raise ValueError("Invalid amino acid sequence: {}".format(query))
 
     else:
         # Amino acid sequences are converted to uppercase
         query = query.upper()
         valid = all(pos in aa_alphabet for pos in query)
         if not valid:
-            print("Invalid amino acid sequence: {}".format(query))
+            raise ValueError("Invalid amino acid sequence: {}".format(query))
 
     return valid
 
 
-def align(query, virus, ref_seq=None, outfile=None):
+def get_ref_seq(ref_seq_path, virus):
     """
-    Pairwise alignment of input sequence relative to HIV reference genome
-    :param query: query sequence
-    :param virus: the reference virus
-    :param ref_seq: reference sequence
-    :param outfile: the file where the output will be written
+    Converts the reference sequence to a string
+    :param ref_seq_path: path to the reference sequence
+    :param virus: the reference virus (HIV or SIV)
+    :return reference_sequence: the reference sequence as a string
     """
 
-    if virus is 'hiv' and ref_seq is None:
+    # TODO: check that it is not a fasta file
+
+    # If no reference sequence is specified, set default reference sequence
+    if virus is 'hiv' and ref_seq_path is None:
         ref_seq = os.path.abspath("ref_genomes/K03455.fasta")
+        print("HIV reference sequence: {}".format(ref_seq))
 
-    if virus is 'siv' and ref_seq is None:
+    if virus is 'siv' and ref_seq_path is None:
         ref_seq = os.path.abspath("ref_genomes/M33262.fasta")
+        print("SIV reference sequence: {}".format(ref_seq))
 
-    # Create a temporary file containing the reference genome and query sequence
-    with open(ref_seq, "r") as ref_handle, tempfile.NamedTemporaryFile('w+', delete=True) as handle:
+    # Get reference sequence
+    with open(ref_seq_path) as ref_handle:
         reference_sequence = ref_handle.read()
+
+    return reference_sequence
+
+
+def align(query, ref_seq, outfile=None):
+    """
+    Sequence locator for 'align mode'
+    :param query: The query sequence
+    :param ref_seq: <option> The reference sequence. If no sequence is specified and HIV is the reference virus,
+                    the reference sequence will be ref_genomes/K03455.fasta. If no sequence is specified and SIV
+                    is the reference virus, the reference sequence will be ref_genomes/M33262.fasta
+    :param outfile: <option> The file where the output will be written.
+                    If no output file is specified, the output will be printed.
+    """
+
+    # Read in reference sequence
+    with open(ref_seq, "r") as ref_handle:
+        reference_sequence = ref_handle.read()
+        print("Reference sequence: {}".format(reference_sequence))
+
+    with tempfile.NamedTemporaryFile('w+', delete=True) as handle:
         handle.write(reference_sequence)
         handle.write('\n>query sequence\n{}\n'.format(query))
+        print("Inside tempfile block... \n "
+              "Reference sequence: {} \n "
+              "Query seqeunce: {} \n".format(reference_sequence, query))
 
-        # Path to the temporary file
+        # Path to the temporary query file
         file_path = os.path.join(tempfile.gettempdir(), handle.name)
 
         raw_output = run_mafft(file_path)
 
     output = raw_output.decode('utf-8')
+    print("Outfile: {}".format(outfile))
 
     if outfile is not None:
         with open(outfile, "w+") as out_handle:
             out_handle.write(output)
-    # Print to console if no outfile is specified
+
+        # Print to console if no outfile is specified
     else:
+        # TODO: print output of alignment to console
         print(output)
 
-# test_path = os.path.abspath("siv-test.fasta")
-# raw_output = run_mafft(test_path)
-# output = raw_output.decode('utf-8')
-# with open("sl-siv-test.txt", "w+") as out_handle:
-#     out_handle.write(output)
+# TODO: write a function that reads output of subprocess and returns the corresponding gene
+#  - sequence_retrieve(start_coord=start of longest alignment, end_coord=end of longest alignment))
+#  - helper function that does not need ref_seq and needs a new outfile
 
 
-def sequence_align(infile, base, virus, ref_seq=None, outfile=None):
-    """
-    Sequence locator for 'align mode'
-    :param infile: input file containing query sequence
-    :param base: base of the sequence (nucleotide or protein)
-    :param virus: the reference virus
-    :param ref_seq: the reference sequence
-    :param outfile: the file where the output will be written
-    """
-
-    with open(infile) as handle:
-        if base == "nucl":
-            query = handle.read().lower()
-        else:
-            query = handle.read().upper()
-
-        # Check if query uses the correct alphabet
-        if valid_sequence(base, query):
-            align(query, virus, ref_seq, outfile)
-        else:
-            raise ValueError("Invalid input")
-
-
-def sequence_retrieve(ref_seq, virus, region, start_coord, end_coord, outfile=None):
+def retrieve(reference_sequence, virus, region, start_coord, end_coord, outfile=None):
     """
     Sequence locator for 'retrieve' mode
-    :param ref_seq: reference genome sequence
+    :param reference_sequence: reference genome sequence
+    :param virus: the reference virus
     :param region: genomic region
     :param start_coord: starting coordinate
     :param end_coord: ending coordinate
@@ -212,23 +221,45 @@ def main():
     args = parse_args()
 
     if args.subcommand == "align":
+        query = args.query
         base = args.base
-        infile = args.infile
         virus = args.virus
         ref_seq = args.refseq
         outfile = args.outfile
 
-        sequence_align(ref_seq, virus, base, infile, outfile)
+        # Read reference sequence from file
+        reference_sequence = get_ref_seq(ref_seq, virus)
 
-    # else:
-    #     region = args.region
-    #     virus = args.virus
-    #     ref_seq = args.refseq
-    #     start_coord = args.first
-    #     end_coord = args.last
-    #     outfile = args.outfile
+        # Get query sequence
+        with open(query) as query_handle:
+            if base == "nucl":
+                query_seq = query_handle.read().lower()
+            else:
+                query_seq = query_handle.read().upper()
+        print("Query file: {}".format(query))
+        print("Query sequence: {}".format(query_seq))
 
-        # sequence_retrieve(ref_seq, virus, region, start_coord, end_coord, outfile)
+        # Check that query and reference sequences use the correct alphabet
+        if valid_sequence(base, query) and valid_sequence(base, reference_sequence):
+            print("Valid sequences")
+            align(query, reference_sequence, outfile)
+
+    else:
+        virus = args.virus
+        base = args.virus
+        ref_seq = args.refseq
+        start_coord = args.first
+        end_coord = args.last
+        region = args.region
+        outfile = args.outfile
+
+        # Read reference_sequence from file
+        reference_sequence = get_ref_seq(ref_seq, virus)
+
+        # Check that reference sequence uses the correct alphabet
+        if valid_sequence(base, reference_sequence):
+            print("Valid sequence")
+            retrieve(reference_sequence, virus, region, start_coord, end_coord, outfile)
 
 
 if __name__ == '__main__':
